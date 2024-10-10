06a56f7f4614b46a2cf38506f29fe1617ad4d123
ethan_zhou <18201521626@139.com>
2024-10-10 15:06:55 +0800

    feat(nvim): enhance color scheme configuration
    - Updated 'catppuccin/nvim' plugin configuration to include additional integrations (mason, nvim_surround, which_key).
    - Improved readability and structure of the color scheme setup.
    - Removed unnecessary whitespace in 'lualine.lua' plugin configuration.

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,7 +1,24 @@
-return {{ 'catppuccin/nvim', name = 'catppuccin', priority = 1000,config=function()
-  vim.cmd.colorscheme "catppuccin"
-end },
-  {'mjlbach/onedark.nvim'}, -- Theme inspired by Atom
-  {'sainnhe/edge'},
+return {
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        integrations = {
+          mason = true,
+          nvim_surround = true,
+          which_key = true,
+        }
+      })
+      vim.cmd.colorscheme "catppuccin"
+    end
+  },
+  {
+    'mjlbach/onedark.nvim', -- Theme inspired by Atom
+  },
+  {
+    'sainnhe/edge',
+  }
 }
 
