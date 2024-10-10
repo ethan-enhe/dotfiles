d03d999df0a155d1f069578a0117ab4026b8e0c4
ethan_zhou <18201521626@139.com>
2024-10-10 14:29:08 +0800

    feat(nvim): improve plugin configuration and key mappings
    - Refactored init.lua to use a safer module loading function.
    - Enhanced key mappings in map.lua with descriptions and additional functionality.
    - Updated Copilot plugin configuration with new prompts and improved setup.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,15 +2,21 @@ require "opt"
 require "map"
 require "plug"
 
--- iterate through and require all .lua plug config files under `./lua/user` directory
-for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/user")) do
-  if file:match("%.lua$") then
-    -- catch any error
-    local ok, err = pcall(require, "user." .. file:sub(1, -5))
-    if not ok then
-      print("Error in user." .. file:sub(1, -5) .. ": " .. err)
-    end
+-- Function to safely require a module and handle errors
+local function safe_require(module)
+  local ok, err = pcall(require, module)
+  if not ok then
+    print("Error in " .. module .. ": " .. err)
   end
 end
 
+-- Get the path to the user configuration directory
+local user_config_path = vim.fn.stdpath("config") .. "/lua/user"
 
+-- Iterate through all .lua files in the user configuration directory
+for _, file in ipairs(vim.fn.readdir(user_config_path)) do
+  if file:match("%.lua$") then
+    local module_name = "user." .. file:sub(1, -5)
+    safe_require(module_name)
+  end
+end
--- .config/nvim/lua/map.lua
+++ .config/nvim/lua/map.lua
@@ -1,64 +1,76 @@
 -- [[ Basic Keymaps ]]
--- maps unrelated to plugins should be in this file
+-- Maps unrelated to plugins should be in this file
+
 -- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+-- NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
 
 -- Keymaps for better default experience
--- See `:help vim.keymap.set()`
+-- Disable space key
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
---
-vim.keymap.set('n', '<C-h>', [[<Cmd>wincmd h<CR>]], { silent = true })
-vim.keymap.set('n', '<C-j>', [[<Cmd>wincmd j<CR>]], { silent = true })
-vim.keymap.set('n', '<C-k>', [[<Cmd>wincmd k<CR>]], { silent = true })
-vim.keymap.set('n', '<C-l>', [[<Cmd>wincmd l<CR>]], { silent = true })
--- vim.keymap.set('i', 'jj', "<esc>", { noremap = true, silent = true })
+-- Window navigation
+vim.keymap.set('n', '<C-h>', '<Cmd>wincmd h<CR>', { silent = true, desc = "Move to left window" })
+vim.keymap.set('n', '<C-j>', '<Cmd>wincmd j<CR>', { silent = true, desc = "Move to bottom window" })
+vim.keymap.set('n', '<C-k>', '<Cmd>wincmd k<CR>', { silent = true, desc = "Move to top window" })
+vim.keymap.set('n', '<C-l>', '<Cmd>wincmd l<CR>', { silent = true, desc = "Move to right window" })
 
+-- Toggle relative line numbers
 vim.keymap.set('n', '<leader>rl', function()
   vim.opt.relativenumber = not vim.opt.relativenumber:get()
-end, { silent = true })
-vim.keymap.set('n', '<leader>w', '<cmd>w!<cr>', { silent = true })
-vim.keymap.set('n', '<leader>s', '<cmd>source %<cr>', { silent = true })
-vim.keymap.set('n', '<leader>cd', '<cmd>cd %:p:h<cr><cmd>pwd<cr>', { silent = true })
-vim.keymap.set('n', '<leader>cf', '<cmd>e $MYVIMRC<cr><cmd>cd %:p:h<cr>', { silent = true })
+end, { silent = true, desc = "Toggle relative line numbers" })
 
-vim.keymap.set('n', '<leader>tt', '<cmd>0 r ~/Documents/mycode/template/other/cf.cpp<cr>', { silent = true })
-vim.keymap.set('n', '<leader>tr', ':r ~/Documents/mycode/template/', { silent = true })
-vim.keymap.set('n', '0', '^')
-vim.keymap.set('x', '0', '^')
+-- Save and source files
+vim.keymap.set('n', '<leader>w', '<cmd>w!<cr>', { silent = true, desc = "Save file" })
+vim.keymap.set('n', '<leader>s', '<cmd>source %<cr>', { silent = true, desc = "Source current file" })
 
+-- Change directory to the current file's directory and print the working directory
+vim.keymap.set('n', '<leader>cd', '<cmd>cd %:p:h<cr><cmd>pwd<cr>', { silent = true, desc = "Change to file's directory and print working directory" })
 
--- Buffers Operations
+-- Edit the Neovim configuration file
+vim.keymap.set('n', '<leader>cf', '<cmd>e $MYVIMRC<cr><cmd>cd %:p:h<cr>', { silent = true, desc = "Edit Neovim configuration file" })
+
+-- Insert template code
+vim.keymap.set('n', '<leader>tt', '<cmd>0 r ~/Documents/mycode/template/other/cf.cpp<cr>', { silent = true, desc = "Insert template code" })
+vim.keymap.set('n', '<leader>tr', ':r ~/Documents/mycode/template/', { silent = true, desc = "Insert template code from directory" })
+
+-- Remap 0 to go to the first non-blank character of the line
+vim.keymap.set('n', '0', '^', { desc = "Go to first non-blank character of the line" })
+vim.keymap.set('x', '0', '^', { desc = "Go to first non-blank character of the line" })
+
+-- Buffer operations
 vim.keymap.set('n', '<leader>bd', function()
-  local curbufnum = vim.fn.bufnr("%")
-  local altbufnum = vim.fn.bufnr("#")
-  if vim.fn.buflisted(altbufnum) then
+  local current_buffer = vim.fn.bufnr("%")
+  local alternate_buffer = vim.fn.bufnr("#")
+  
+  if vim.fn.buflisted(alternate_buffer) then
     vim.cmd("buffer #")
   else
     vim.cmd("bnext")
   end
 
-  if vim.fn.bufnr("%") == curbufnum then
+  if vim.fn.bufnr("%") == current_buffer then
     vim.cmd("new")
   end
-  if vim.fn.buflisted(curbufnum) then
-    vim.cmd("bdelete! " .. curbufnum)
-  end
-end, { silent = true })
-vim.keymap.set('n', '<leader>l', '<cmd>bnext<cr>', { silent = true })
-vim.keymap.set('n', '<leader>h', '<cmd>bprevious<cr>', { silent = true })
-vim.keymap.set('n', '<leader><cr>', '<cmd>noh<cr>', { silent = true })
 
+  if vim.fn.buflisted(current_buffer) then
+    vim.cmd("bdelete! " .. current_buffer)
+  end
+end, { silent = true, desc = "Delete current buffer" })
+
+vim.keymap.set('n', '<leader>l', '<cmd>bnext<cr>', { silent = true, desc = "Go to next buffer" })
+vim.keymap.set('n', '<leader>h', '<cmd>bprevious<cr>', { silent = true, desc = "Go to previous buffer" })
+
+-- Clear search highlighting
+vim.keymap.set('n', '<leader><cr>', '<cmd>noh<cr>', { silent = true, desc = "Clear search highlighting" })
 
 -- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
+-- Highlight text on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
