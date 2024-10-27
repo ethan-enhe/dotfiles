acbd0c91072d12f0ad32d198237ab8c3cd4b6138
ethan_zhou <18201521626@139.com>
2024-10-27 09:28:16 +0800

    new conf

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,32 +1,25 @@
+local preview_config = {
+  type = "float",
+  relative = "editor",
+  border = "rounded",
+  title = "Preview",
+  title_pos = "center",
+  position = { 0, -2 },
+  size = { width = 0.3, height = 0.3 },
+  zindex = 200,
+}
+
 return {
   "folke/trouble.nvim",
   opts = {
     modes = {
       preview_float = {
         mode = "diagnostics",
-        preview = {
-          type = "float",
-          relative = "editor",
-          border = "rounded",
-          title = "Preview",
-          title_pos = "center",
-          position = { 0, -2 },
-          size = { width = 0.3, height = 0.3 },
-          zindex = 200,
-        },
+        preview = preview_config,
       },
       qf_preview_float = {
         mode = "quickfix",
-        preview = {
-          type = "float",
-          relative = "editor",
-          border = "rounded",
-          title = "Preview",
-          title_pos = "center",
-          position = { 0, -2 },
-          size = { width = 0.3, height = 0.3 },
-          zindex = 200,
-        },
+        preview = preview_config,
       },
     },
   }, -- for default options, refer to the configuration section for custom setup.
---
+++ .zshenv
@@ -0,0 +1,8 @@
+# export GDK_SCALE=2
+# export GDK_DPI_SCALE=0.5
+export EDITOR='nvim'
+export PATH=$PATH:$HOME/Documents/mycode/scripts:$HOME/.local/share/gem/ruby/3.0.0/bin:/opt/gcc-arm-none-eabi-10.3-2021.10/bin/:/opt/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-elf/bin/:$HOME/.cargo/bin
+# export PATH=$PATH:$HOME/Documents/mycode/scripts:$HOME/.local/share/gem/ruby/3.0.0/bin
+# export LD_PRELOAD=/usr/lib/libasan.so:$LD_PRELOAD
+export LIBVA_DRIVER_NAME=iHD
+export GTK_USE_PORTAL=1
