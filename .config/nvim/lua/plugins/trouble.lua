5e8c3a8ec2ffdf38e8e899d06c6f667d5d5742c5
ethan_zhou <18201521626@139.com>
2024-11-15 16:45:16 +0800

    flashnvim

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,71 +1,56 @@
 return {}
-
---
--- local preview_config = {
---   type = "float",
---   relative = "editor",
---   border = "rounded",
---   title = "Preview",
---   title_pos = "center",
---   position = { 0, -2 },
---   size = { width = 0.3, height = 0.3 },
---   zindex = 200,
--- }
--- -- vim.api.nvim_create_autocmd("QuickFixCmdPost", {
--- --   callback = function()
--- --     vim.cmd([[Trouble qflist open]])
--- --   end,
--- -- })
---
 -- return {
---   "folke/trouble.nvim",
---   opts = {
---     modes = {
---       preview_float = {
---         --auto_close = true, -- auto close when there are no items
---         --auto_open = true, -- auto open when there are items
---         mode = "diagnostics",
---         preview = preview_config,
---       },
---       qf_preview_float = {
---         auto_close = true, -- auto close when there are no items
---         auto_open = true, -- auto open when there are items
---         mode = "quickfix",
---         preview = preview_config,
---       },
+--     "folke/trouble.nvim",
+--     cmd = "Trouble",
+--     keys = {
+--         {
+--             "<leader>xx",
+--             "<cmd>Trouble diagnostics toggle<cr>",
+--             desc = "Diagnostics (Trouble)",
+--         },
+--         {
+--             "<leader>xX",
+--             "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+--             desc = "Buffer Diagnostics (Trouble)",
+--         },
+--         {
+--             "<leader>cs",
+--             "<cmd>Trouble symbols toggle focus=false<cr>",
+--             desc = "Symbols (Trouble)",
+--         },
+--         {
+--             "<leader>cl",
+--             "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+--             desc = "LSP Definitions / references / ... (Trouble)",
+--         },
+--         {
+--             "<leader>xL",
+--             "<cmd>Trouble loclist toggle<cr>",
+--             desc = "Location List (Trouble)",
+--         },
+--         {
+--             "<leader>xQ",
+--             "<cmd>Trouble qflist toggle<cr>",
+--             desc = "Quickfix List (Trouble)",
+--         },
 --     },
---   }, -- for default options, refer to the configuration section for custom setup.
---   cmd = "Trouble",
---   keys = {
---     {
---       "<leader>xx",
---       "<cmd>Trouble preview_float toggle<cr>",
---       desc = "Diagnostics (Trouble)",
---     },
---     {
---       "<leader>xX",
---       "<cmd>Trouble preview_float toggle filter.buf=0<cr>",
---       desc = "Buffer Diagnostics (Trouble)",
---     },
---     {
---       "<leader>xs",
---       "<cmd>Trouble symbols toggle focus=false<cr>",
---       desc = "Symbols (Trouble)",
---     },
---     {
---       "<leader>xl",
---       "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
---       desc = "LSP Definitions / references / ... (Trouble)",
---     },
---     {
---       "<leader>xL",
---       "<cmd>Trouble loclist toggle<cr>",
---       desc = "Location List (Trouble)",
---     },
---     {
---       "<leader>xq",
---       "<cmd>Trouble qf_preview_float toggle<cr>",
---       desc = "Quickfix List (Trouble)",
---     },
---   },
+--     config = function()
+--         require("trouble").setup {
+--         }
+--         vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+--             callback = function()
+--                 vim.cmd([[Trouble qflist open]])
+--             end,
+--         })
+--         vim.api.nvim_create_autocmd("BufRead", {
+--             callback = function(ev)
+--                 if vim.bo[ev.buf].buftype == "quickfix" then
+--                     vim.schedule(function()
+--                         vim.cmd([[cclose]])
+--                         vim.cmd([[Trouble qflist open]])
+--                     end)
+--                 end
+--             end,
+--         })
+--     end
 -- }
