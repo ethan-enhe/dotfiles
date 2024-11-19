01ace3c6a9db91812edc7afbbfd59b39cf51bef2
ethan_zhou <18201521626@139.com>
2024-11-19 16:25:15 +0800

    feat: update vimtex config and gitconfig, adjust latexmkrc
    - Updated vimtex.lua to include additional quickfix ignore filters.
    - Added new directory to .gitconfig for CV.
    - Adjusted .latexmkrc settings for pdf_mode and commented out pdflatex.

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -3,7 +3,7 @@ return {
     config = function()
         -- vim.g.vimtex_quickfix_mode=0
         -- vim.g.vimtex_quickfix_ignore_filters = {'Underfull','Overfull','Package fontspec Warning'}
-        vim.g.vimtex_quickfix_ignore_filters = { 'Warning' }
+        vim.g.vimtex_quickfix_ignore_filters = { 'Warning', 'Underfull', 'Overfull', 'Package fontspec Warning' }
         vim.opt.conceallevel = 1
         vim.g.tex_conceal = 'abdmg'
         vim.g.vimtex_view_method = "zathura"
--- .latexmkrc
+++ .latexmkrc
@@ -2,5 +2,5 @@
 # $pdflatex = "xelatex -pvc -file-line-error -interaction=nonstopmode";
 # $pdf_previewer = 'start /usr/bin/okular %S';
 # $preview_continuous_mode = 1;
-$pdf_mode = 5;
-$pdflatex = 'pdflatex --shell-escape %O %S';
+$pdf_mode = 1;
+# $pdflatex = 'pdflatex --shell-escape %O %S';
